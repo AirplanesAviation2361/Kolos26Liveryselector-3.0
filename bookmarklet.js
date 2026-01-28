24044f2e0f2a988f50e8ea66e8dd6b597db90dca
Airplanesaviation2361 <karan.kuppa@icloud.com>
2026-01-28 17:17:52 +0400

    Update and rename Consistent bookmark code to  bookmarklet.js

---
+++ bookmarklet.js
@@ -0,0 +1,2 @@
+
+javascript:(function()%7Bfunction%20callback()%7Balert(%22test!%22)%7Dvar%20s%3Ddocument.createElement(%22script%22)%3Bs.src%3D%22https%3A%2F%2Fgithub.com%2FAirplanesAviation2361%2FKolos26Liveryselector-3.0.git%22%3Bif(s.addEventListener)%7Bs.addEventListener(%22load%22%2Ccallback%2Cfalse)%7Delse%20if(s.readyState)%7Bs.onreadystatechange%3Dcallback%7Ddocument.body.appendChild(s)%3B%7D)()
