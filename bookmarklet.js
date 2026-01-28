2607afef571997c5cc871a462a2d3f08ccae6698
Airplanesaviation2361 <karan.kuppa@icloud.com>
2026-01-28 17:20:25 +0400

    Update bookmarklet.js

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,2 +1,2 @@
+javascript:(function(){function callback(){alert("Test!");}var s=document.createElement('script');s.src="https://raw.githack.com/AirplanesAviation2361/GEOFS-LiverySelector3.0/master/main.js";if(s.addEventListener){s.addEventListener("load",callback,false);}else if(s.readyState){s.onreadystatechange=callback;}document.body.appendChild(s);})();
 
-javascript:(function()%7Bfunction%20callback()%7Balert(%22test!%22)%7Dvar%20s%3Ddocument.createElement(%22script%22)%3Bs.src%3D%22https%3A%2F%2Fgithub.com%2FAirplanesAviation2361%2FKolos26Liveryselector-3.0.git%22%3Bif(s.addEventListener)%7Bs.addEventListener(%22load%22%2Ccallback%2Cfalse)%7Delse%20if(s.readyState)%7Bs.onreadystatechange%3Dcallback%7Ddocument.body.appendChild(s)%3B%7D)()
